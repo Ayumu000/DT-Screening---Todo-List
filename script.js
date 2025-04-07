0ba50d3da644a7deeb70bda36b4c17e02503b634
Ayumu000 <172831717+Ayumu000@users.noreply.github.com>
2025-04-07 18:38:59 +0800

    Added the app structure and its stylings, and selecting necessary event listeners in JS script

---
+++ script.js
@@ -0,0 +1,12 @@
+function submit(e) {}
+
+const task_list = document.querySelectorAll(".task-list");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const add = document.querySelector(".add");
+const remove = document.querySelector(".remove");
+
+form.addEventListener("submit", (e) => {});
+add.addEventListener("click", (e) => {});
+remove.addEventListener("click", (e) => {});
+input.addEventListener("keyup", (e) => {});
