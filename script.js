6618a92080c5674821de46ec50e73875fc7f1c19
Ayumu000 <172831717+Ayumu000@users.noreply.github.com>
2025-04-07 19:37:19 +0800

    Added the task app functionality

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-function submit(e) {}
+function submit(e) {
+  e.preventDefault();
+  const task = input_add.value;
+  input_add.value = ""; // Clear
 
-const task_list = document.querySelectorAll(".task-list");
+  add_task(task);
+}
+
+function add_task(task) {
+  const id = tasks.length + 1;
+
+  tasks.push(id);
+  task_list.innerHTML += `
+  <div class="task task_${id}">
+    <span class="mark"></span>
+    <input type="text" placeholder="${task}" disabled />
+    <button class="remove">Delete</button>
+  </div>`;
+
+  const task_list_items = document.querySelectorAll(".task");
+
+  Array.from(task_list_items).forEach((task) => {
+    task.addEventListener("click", (e) => {
+      task.classList.toggle("checked");
+    });
+
+    task.querySelector(".remove").addEventListener("click", (e) => {
+      task.remove();
+    });
+  });
+}
+
+const task_list = document.querySelector(".task-list");
 const form = document.querySelector("form");
-const input = document.querySelector("input");
-const add = document.querySelector(".add");
-const remove = document.querySelector(".remove");
+const input_add = document.querySelector(".input-add");
 
-form.addEventListener("submit", (e) => {});
-add.addEventListener("click", (e) => {});
-remove.addEventListener("click", (e) => {});
-input.addEventListener("keyup", (e) => {});
+const tasks = [];
+
+form.addEventListener("submit", (e) => {
+  submit(e);
+});
